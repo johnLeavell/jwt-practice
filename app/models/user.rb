bf32b7af0c4bf546f4ac174bc177570b4e306922
John Leavell <johnleavell@gmail.com>
2020-05-22 10:18:52 -0500

    updated user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
     validates :username, uniqueness: { case_sensitive: false }
+    validates :password, length: {
+        min: 7,
+        mad: 20,
+    }
 end
