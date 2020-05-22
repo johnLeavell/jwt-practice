6b7fa84be7aa0ed3bda1c50811e90ffa22c30fed
John Leavell <johnleavell@gmail.com>
2020-05-22 11:06:38 -0500

    verfies identity

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,2 +1,17 @@
 class AuthController < ApplicationController
+    def login
+        user = User.find_by(username: params[:username])
+
+        if !user
+            render status: :unauthorized
+        else
+            if user.authenticate(params[:password])
+                render json: {
+                    message: "You did it!"
+                }
+            else
+                render status: :unauthorized
+            end
+        end
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
+
     validates :username, uniqueness: { case_sensitive: false }
     validates :password, length: {
-        min: 7,
-        mad: 20,
+        min: 6,
+        max: 20,
     }
 end
