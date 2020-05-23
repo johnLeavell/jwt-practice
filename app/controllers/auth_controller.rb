ad21897f1e30c6938562d886617889d71eff6ff6
John Leavell <johnleavell@gmail.com>
2020-05-23 07:05:23 -0500

    protected data

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -8,8 +8,7 @@ class AuthController < ApplicationController
             if user.authenticate(params[:password])
                 secret_key = Rails.application.secrets.secret_key_base[0]
                 token = JWT.encode({
-                    user_id: user.id,
-                    username: user.username,
+                    user_id: user.id
                 }, secret_key)
 
                 render json: {
--- app/controllers/balloons_controller.rb
+++ app/controllers/balloons_controller.rb
@@ -1,4 +1,5 @@
 class BalloonsController < ApplicationController
+    before_action :authenticate, only: [:create] 
     def index
         balloons = Balloon.all
 
@@ -6,24 +7,12 @@ class BalloonsController < ApplicationController
     end
 
     def create
-        authorization_header = request.headers[:authorization]
-        if !authorization_header
-            render status: :unauthorized
-        else
-            token = authorization_header.split(" ")[1]
-            secret_key = Rails.application.secrets.secret_key_base[0]
-            decoded_token = JWT.decode(token, secret_key)
+        balloon = Balloon.create(
+            name: params[:name],
+            user: @user
+        )
 
-            # byebug
-            user = User.find(decoded_token[0]["user_id"])
-
-            balloon = Balloon.create(
-                name: params[:name],
-                user: user
-            )
-
-            render json: { balloon: balloon }
-        end
-        
+        render json: { balloon: balloon }
     end
+        
 end
