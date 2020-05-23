7b14e1d4727b3ec133cab7234ed842f13f7edb41
John Leavell <johnleavell@gmail.com>
2020-05-23 06:37:16 -0500

    created assoications

--- db/schema.rb
+++ db/schema.rb
@@ -17,8 +17,10 @@ ActiveRecord::Schema.define(version: 2020_05_22_185246) do
 
   create_table "balloons", force: :cascade do |t|
     t.string "name"
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_balloons_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,4 +30,5 @@ ActiveRecord::Schema.define(version: 2020_05_22_185246) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "balloons", "users"
 end
